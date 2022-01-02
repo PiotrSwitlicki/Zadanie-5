5da5b115688d3bdf7c4f0febacba027d7183e282
PiotrSwitlicki <75681208+PiotrSwitlicki@users.noreply.github.com>
2022-01-02 23:06:46 +0100

    Update InvoiceController.php

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -26,7 +26,7 @@ class InvoiceController extends Controller
      */
     public function create()
     {
-        return view('invoices.create')->with('popup', 'open');;
+        return view('invoices.create');
     }
 
     /**
